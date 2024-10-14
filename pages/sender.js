e43efc41ec36c30de975c23cd6cd6ecda5a293a7
will wade <willwade@gmail.com>
2024-10-14 09:01:57 +0200

    Update sender.js

--- pages/sender.js
+++ pages/sender.js
@@ -1,11 +1,20 @@
 import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 import WebRTCService from '../services/WebRTCService';
 
 const SenderApp = () => {
+  const router = useRouter();
   const [webrtcService, setWebrtcService] = useState(null);
   const [message, setMessage] = useState('');
   const [sessionId, setSessionId] = useState('');
   const websocketURL = process.env.NEXT_PUBLIC_WS_URL;
+
+  // Automatically set sessionId from URL if available
+  useEffect(() => {
+    if (router.query.sessionId) {
+      setSessionId(router.query.sessionId);
+    }
+  }, [router.query.sessionId]);
 
   useEffect(() => {
     if (sessionId) { // Connect only if session ID is provided
