99336106bd992100c4b7346123dd692b6f4b47a3
will wade <willwade@gmail.com>
2024-10-14 05:21:35 +0200

    Remove styles

--- components/DisplayText.js
+++ components/DisplayText.js
@@ -1,7 +1,6 @@
 
         import React from 'react';
         import { TypeAnimation } from 'react-type-animation';
-        import '../public/styles.css';
 
         const DisplayText = ({ text, fontSize, color, speed, lines }) => (
             <div id="display-container" style={{ fontSize, color, lineHeight: `${fontSize * 1.2}px`, overflow: 'hidden', maxHeight: `${fontSize * lines * 1.2}px` }}>
