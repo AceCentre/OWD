55aa4ac8cad2b2db6bb334043378778a23def34d
will wade <willwade@gmail.com>
2024-10-14 05:25:21 +0200

    Update QRCodeDisplay.js

--- components/QRCodeDisplay.js
+++ components/QRCodeDisplay.js
@@ -1,6 +1,6 @@
 
         import React from 'react';
-        import { QRCode } from 'qrcode.react';
+        import { QRCodeCanvas } from 'qrcode.react';
 
         const QRCodeDisplay = ({ websocketURL }) => (
             <div className="qr-code">
