f29ad41276644e5a8f37e63d8611298ee580886c
will wade <willwade@gmail.com>
2024-10-14 07:37:26 +0200

    Add uniqid

--- services/WebRTCService.js
+++ services/WebRTCService.js
@@ -6,24 +6,32 @@ class WebRTCService {
         this.channel.onmessage = (event) => this.onMessageReceived(event.data);
     }
 
-    async createOffer() {
+    async createOffer(sessionId) {
         const offer = await this.peerConnection.createOffer();
         await this.peerConnection.setLocalDescription(offer);
-        // Use WebSocket signaling if necessary
+        // Send the offer along with the session ID to the signaling server
         if (this.socket && this.socket.readyState === WebSocket.OPEN) {
-            this.socket.send(JSON.stringify({ type: 'offer', data: offer }));
+            this.socket.send(JSON.stringify({ type: 'offer', sessionId, data: offer }));
         }
     }
 
-    connect(websocketURL) {
-        this.socket = new WebSocket(websocketURL);
+    connect(websocketURL, sessionId) {
+        // Include session ID in the WebSocket URL if necessary
+        const urlWithSession = `${websocketURL}?sessionId=${sessionId}`;
+        this.socket = new WebSocket(urlWithSession);
+        
         this.socket.onmessage = (event) => {
             const message = JSON.parse(event.data);
             if (message.type === 'answer') {
                 this.peerConnection.setRemoteDescription(new RTCSessionDescription(message.data));
             }
         };
-        this.socket.onopen = () => console.log('WebSocket connection established');
+        
+        this.socket.onopen = () => {
+            console.log('WebSocket connection established');
+            // Create offer after establishing the WebSocket connection
+            this.createOffer(sessionId);
+        };
     }
 
     async sendMessage(message) {
