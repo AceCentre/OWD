048c9f58149367c4b8b5803d4305cfb1096aacec
will wade <willwade@gmail.com>
2024-10-14 09:00:35 +0200

    Update QRCodeDisplay.js

--- components/QRCodeDisplay.js
+++ components/QRCodeDisplay.js
@@ -1,7 +1,7 @@
 import { QRCodeCanvas } from 'qrcode.react';
 
 const QRCodeDisplay = ({ sessionId }) => {
-    const displayURL = `${process.env.NEXT_PUBLIC_BASE_URL}/connect/${sessionId}`;
+    const displayURL = `${process.env.NEXT_PUBLIC_BASE_URL}/sender?sessionId=${sessionId}`;
     
     return (
         <div className="qr-code">
