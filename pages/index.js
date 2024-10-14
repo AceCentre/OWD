b72fe52807d69daf7ca0b0b69e163fd593a28e06
will wade <willwade@gmail.com>
2024-10-14 07:05:29 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,22 +8,16 @@ import BLEService from '../services/BLEService';
 const Home = () => {
   const [text, setText] = useState('Waiting for messages...');
   const [settings, setSettings] = useState({ fontSize: '24px', color: '#000', speed: 50, lines: 3 });
-  const [webrtcService, setWebrtcService] = useState(null);
-  const [bleService, setBleService] = useState(null);
-
-  // Use WebSocket URL from the environment variable
   const websocketURL = process.env.NEXT_PUBLIC_WS_URL;
 
   useEffect(() => {
     const webrtc = new WebRTCService(setText);
-    webrtc.createOffer();
     webrtc.connect(websocketURL);
-    setWebrtcService(webrtc);
+    webrtc.createOffer();
 
     if ('bluetooth' in navigator) {
       const ble = new BLEService(setText);
       ble.connect();
-      setBleService(ble);
     }
   }, [websocketURL]);
 
