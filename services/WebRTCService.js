5968bb7f509b632c625750a3d547aa73a82cb26a
will wade <willwade@gmail.com>
2024-10-14 07:04:46 +0200

    Update WebRTCService.js

--- services/WebRTCService.js
+++ services/WebRTCService.js
@@ -1,23 +1,36 @@
+class WebRTCService {
+    constructor(onMessageReceived) {
+        this.onMessageReceived = onMessageReceived;
+        this.peerConnection = new RTCPeerConnection();
+        this.channel = this.peerConnection.createDataChannel('messaging');
+        this.channel.onmessage = (event) => this.onMessageReceived(event.data);
+    }
 
-        class WebRTCService {
-            constructor(onMessageReceived) {
-                this.onMessageReceived = onMessageReceived;
-                this.peerConnection = new RTCPeerConnection();
-                this.channel = this.peerConnection.createDataChannel('messaging');
-                this.channel.onmessage = (event) => this.onMessageReceived(event.data);
-            }
-
-            async createOffer() {
-                const offer = await this.peerConnection.createOffer();
-                await this.peerConnection.setLocalDescription(offer);
-            }
-
-            async sendMessage(message) {
-                if (this.channel.readyState === 'open') {
-                    this.channel.send(message);
-                }
-            }
+    async createOffer() {
+        const offer = await this.peerConnection.createOffer();
+        await this.peerConnection.setLocalDescription(offer);
+        // Use WebSocket signaling if necessary
+        if (this.socket && this.socket.readyState === WebSocket.OPEN) {
+            this.socket.send(JSON.stringify({ type: 'offer', data: offer }));
         }
+    }
 
-        export default WebRTCService;
-    
+    connect(websocketURL) {
+        this.socket = new WebSocket(websocketURL);
+        this.socket.onmessage = (event) => {
+            const message = JSON.parse(event.data);
+            if (message.type === 'answer') {
+                this.peerConnection.setRemoteDescription(new RTCSessionDescription(message.data));
+            }
+        };
+        this.socket.onopen = () => console.log('WebSocket connection established');
+    }
+
+    async sendMessage(message) {
+        if (this.channel.readyState === 'open') {
+            this.channel.send(message);
+        }
+    }
+}
+
+export default WebRTCService;
