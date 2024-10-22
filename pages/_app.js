c0dcd48688c79b1370d74755450c3fa741c1c0f0
will wade <willwade@gmail.com>
2024-10-22 05:48:34 +0100

    fixed google font

--- pages/_app.js
+++ pages/_app.js
@@ -3,22 +3,10 @@ import "../styles/globals.css";
 import "../styles/Home.css";
 import App from "next/app";
 
-import Head from "next/head";
-
 function MyApp({ Component, pageProps }) {
-    return (
-        <>
-            <Head>
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&family=Oswald:wght@200..700&family=Permanent+Marker&display=swap"
-                    rel="stylesheet"
-                />
-            </Head>
-            <Component {...pageProps} />
-        </>
-    );
+    return <Component {...pageProps} />;
 }
 
 export default MyApp;
+
+
