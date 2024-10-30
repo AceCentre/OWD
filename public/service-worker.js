44c92fd9dc8c19413655fc98741629d826105e6d
will wade <willwade@gmail.com>
2024-10-30 22:21:12 +0000

    add logging and remove index.html from cache

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,9 +1,13 @@
 const CACHE_NAME = "aac-display-cache";
-const urlsToCache = ["/", "/index.html", "/manifest.json"];
+const urlsToCache = ["/", "/manifest.json"];
 
 self.addEventListener("install", (event) => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(urlsToCache).catch((error) => {
+                console.error("Failed to cache:", error);
+            });
+        })
     );
 });
 
