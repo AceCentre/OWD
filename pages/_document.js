847ad07378a317ba0c5abb21db25cfa3040616c2
will wade <willwade@gmail.com>
2024-10-30 22:10:08 +0000

    Correct icons for safari

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,8 @@ export default function Document() {
     return (
         <Html>
             <Head>
+                <link rel="icon" type="image/png" sizes="32x32" href="/AppImages/ios/32.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/AppImages/ios/16.png" />
                 <link rel="manifest" href="/manifest.json" />
                 <meta name="theme-color" content="#000000" />
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -17,17 +19,17 @@ export default function Document() {
                     href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&family=Oswald:wght@200..700&family=Permanent+Marker&display=swap"
                     rel="stylesheet"
                 />
-                <link rel="apple-touch-icon" sizes="180x180" href="/AppImages/ios/icon-180x180.png" />
-                <link rel="apple-touch-icon" sizes="152x152" href="/AppImages/ios/icon-152x152.png" />
-                <link rel="apple-touch-icon" sizes="120x120" href="/AppImages/ios/icon-120x120.png" />
-                <link rel="apple-touch-icon" sizes="76x76" href="/AppImages/ios/icon-76x76.png" />
-                <link rel="apple-touch-icon" href="/AppImages/ios/icon-192x192.png" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/AppImages/ios/180.png" />
+                <link rel="apple-touch-icon" sizes="152x152" href="/AppImages/ios/152.png" />
+                <link rel="apple-touch-icon" sizes="120x120" href="/AppImages/ios/120.png" />
+                <link rel="apple-touch-icon" sizes="76x76" href="/AppImages/ios/76.png" />
+                <link rel="apple-touch-icon" href="/AppImages/ios/192.png" />
 
-                <link rel="apple-touch-icon-precomposed" sizes="180x180" href="/AppImages/ios/icon-180x180.png" />
-                <link rel="apple-touch-icon-precomposed" sizes="152x152" href="/AppImages/ios/icon-152x152.png" />
-                <link rel="apple-touch-icon-precomposed" sizes="120x120" href="/AppImages/ios/icon-120x120.png" />
-                <link rel="apple-touch-icon-precomposed" sizes="76x76" href="/AppImages/ios/icon-76x76.png" />
-                <link rel="apple-touch-icon-precomposed" href="/AppImages/ios/icon-192x192.png" />
+                <link rel="apple-touch-icon-precomposed" sizes="180x180" href="/AppImages/ios/180.png" />
+                <link rel="apple-touch-icon-precomposed" sizes="152x152" href="/AppImages/ios/152.png" />
+                <link rel="apple-touch-icon-precomposed" sizes="120x120" href="/AppImages/ios/120.png" />
+                <link rel="apple-touch-icon-precomposed" sizes="76x76" href="/AppImages/ios/76.png" />
+                <link rel="apple-touch-icon-precomposed" href="/AppImages/ios/192.png" />
             </Head>
             <body>
                 <Main />
